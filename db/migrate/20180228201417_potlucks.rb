9ff474806d2c3b4e9d2786cd114e33592b0ec1eb
github username <github email address>
2018-02-28 14:36:57 -0600

    create tables potluck, potluck recipes, recipes

--- db/migrate/20180228201417_potlucks.rb
+++ db/migrate/20180228201417_potlucks.rb
@@ -1,7 +1,12 @@
 class Potlucks < ActiveRecord::Migration[5.1]
   def change
     create_table :potlucks do |t|
-
+      t.string :name
+      t.string :location
+      t.string :description
+      t.datetime :happening
+      t.string :image
+      t.integer :host_id
       t.timestamps
     end
   end
--- db/migrate/20180228201457_potluck_recipes.rb
+++ db/migrate/20180228201457_potluck_recipes.rb
@@ -1,7 +1,8 @@
 class PotluckRecipes < ActiveRecord::Migration[5.1]
   def change
     create_table :potluck_recipes do |t|
-
+      t.integer :potluck_id
+      t.integer :recipe_id
       t.timestamps
     end
   end
