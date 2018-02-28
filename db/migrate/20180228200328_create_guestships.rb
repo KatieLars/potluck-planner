5763cec0054fb6acd40be394d3d0a71246806604
github username <github email address>
2018-02-28 14:39:51 -0600

    add columns guestship

--- db/migrate/20180228200328_create_guestships.rb
+++ db/migrate/20180228200328_create_guestships.rb
@@ -1,7 +1,8 @@
 class CreateGuestships < ActiveRecord::Migration[5.1]
   def change
     create_table :guestships do |t|
-
+      t.integer :guest_id
+      t.integer :potluck_id
       t.timestamps
     end
   end
