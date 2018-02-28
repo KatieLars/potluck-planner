20e6e6d3f79ab5df1da05f87c83527c4be98fd9d
github username <github email address>
2018-02-28 16:49:11 -0600

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :guestships, foreign_key: :guest_id
-  had_many :potlucks, through: :guestships
+  has_many :potlucks, through: :guestships
   has_many :recipes, through: :potlucks
   has_many :friendships
   has_many :friends,through: :frienships
