643ddfa1c6fb720b320017f33d0e70af5e195b05
github username <github email address>
2018-02-28 14:52:05 -0600

    controller model mix up fixed

--- app/controllers/guestships_controller.rb
+++ app/controllers/guestships_controller.rb
@@ -1,4 +1,2 @@
 class Guestships < ApplicationController
-  belongs_to :potluck
-  belongs_to :guest, classname :user
 end
--- app/models/guestship.rb
+++ app/models/guestship.rb
@@ -1,2 +1,4 @@
 class Guestship < ApplicationRecord
+  belongs_to :potluck
+  belongs_to :guest, classname :user
 end
