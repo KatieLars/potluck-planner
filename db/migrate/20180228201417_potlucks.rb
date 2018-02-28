658a555a1a6afae384cf956866d26c82729ea07f
github username <github email address>
2018-02-28 14:30:22 -0600

    update potluck and join change

--- db/migrate/20180228201417_potlucks.rb
+++ db/migrate/20180228201417_potlucks.rb
@@ -1,4 +1,8 @@
 class Potlucks < ActiveRecord::Migration[5.1]
   def change
+    create_table :potlucks do |t|
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20180228201457_potluck_recipes.rb
+++ db/migrate/20180228201457_potluck_recipes.rb
@@ -1,4 +1,8 @@
 class PotluckRecipes < ActiveRecord::Migration[5.1]
   def change
+    create_table :potluck_recipes do |t|
+
+      t.timestamps
+    end
   end
 end
