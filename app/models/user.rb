9aa5a642c19c9829f8f5e8891d155179f45fa131
github username <github email address>
2018-02-28 14:59:53 -0600

    create user asssociations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :potlucks, through: :guestships
+  has_many :recipes, through: :potlucks
+  has_many :friendships
+  has_many :friends,through: :frienships
 end
