8ad110d34d3f9081fffcb894eab9fe644220bc59
github username <github email address>
2018-02-28 18:48:44 -0600

    create potluck methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :guestships, foreign_key: :guest_id
-  has_many :potlucks
-  has_many :guest_potlucks, through: :guestships, source: :potlucks, foreign_key: :potluck_id
+  has_many :potlucks #these are the potlucks the user hosts
   has_many :recipes, through: :potlucks
   has_many :friendships
   has_many :friends, through: :friendships
+
+  def guest_potlucks #returns a list of potlucks where the person is a guest
+    self.guestships.collect {|p| p.potluck}
+  end
+
+  def total_potlucks #returns a list of total potlucks
+    self.guest_potlucks.push(self.potlucks)
+  end
+
+
 end
