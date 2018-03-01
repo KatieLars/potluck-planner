200879fb1abd88b277e2bd3dff27bc44b3f21457
github username <github email address>
2018-02-28 19:54:36 -0600

    set up api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # scope '/api' do
-  #   get :food, to: 'foods#index'
-  # end
+   scope '/api' do
+     resources :potlucks
+     resources :users
+     resources :guestships
+     resources :friendships
+     resources :recipes
+   end
 end
