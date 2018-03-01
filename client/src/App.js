1fb285c694c6711799bcb80402ad3d2f193e14e7
github username <github email address>
2018-03-01 15:24:21 -0600

    debugging bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, Container} from 'reactstrap'
 import WelcomeNavbar from './containers/welcomeNavbar'
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 
--- client/src/containers/welcomeNavbar.js
+++ client/src/containers/welcomeNavbar.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { NavLink as TopNav } from 'react-router-dom';
-import {NavLink, Navbar, NavItem} from 'reactstrap'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 
 class WelcomeNavbar extends React.Component{
   render() {
     return (
-      <Navbar>
-          <NavItem><TopNav
-              to="/about"
-              exact
-            >About</TopNav>
-          </NavItem>
-          <NavItem>
-            <TopNav
-              to="/login"
-              exact
-            >Log In</TopNav>
-          </NavItem>
-          <NavItem>
-            <TopNav
-              to="/signup"
-              exact
-            >Sign Up</TopNav>
-          </NavItem>
+      <div>
+      <Navbar color="faded" light toggleable>
+        <NavbarToggler right />
+        <NavbarBrand href="/">BeautyPlaces</NavbarBrand>
+        <Collapse navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink><TopNav
+                  to="/about"
+                  exact
+                >About</TopNav></NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
+    </div>
     )
   }
 }
