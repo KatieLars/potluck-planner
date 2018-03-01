6d08b99b0f300640355ccd68796f730062d36a22
github username <github email address>
2018-03-01 13:46:48 -0600

    import elements from reactstrap

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
+import { Button,
+   Col,
+   Row,
+   Container,
+   NavLink,
+   NavItem,
+   NavbarBrand,
+   Navbar,
+   Nav,
+  Collapse} from 'reactstrap'
 //import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from
