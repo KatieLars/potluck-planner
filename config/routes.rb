f43cd513ae06735881967ab3e18afeeda1bb9275
github username <github email address>
2018-03-02 17:02:48 -0600

    mount knock

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
    scope '/api' do
      resources :potlucks
      resources :users
+     mount Knock::Engine => "/knock"
      resources :guestships
      resources :friendships
      resources :recipes
