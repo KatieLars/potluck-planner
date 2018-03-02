9cb8c4152e4eb0fda9ed50280ca1fc5e5bae7462
github username <github email address>
2018-03-02 14:21:36 -0600

    finish landing page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron, Button } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
+// import "../images/Potluck.jpg"
+//
+// const potluck = {
+//   backgroundImage: "../images/Potluck.jpg"
+// }
 
 const Home = () =>{
   return(
     <div>
       <Jumbotron style={{textAlign: "center"}}>
-
-        <h1>Welcome!</h1>
+        <h1>Welcome</h1>
+        <p className="lead"><em>What's happening, who's coming, and what's to eat!</em></p>
         <hr class="my-4"/>
-
+        <p>Set a date. Invite friends. Bring food. And track it all.</p>
+        <Button><NavLink to="/about">Learn More</NavLink></Button>
       </Jumbotron>
     </div>
   )
