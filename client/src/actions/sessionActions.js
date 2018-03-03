777252625992ff5ba03f8195831a12e703aed0d9
github username <github email address>
2018-03-02 18:18:53 -0600

    ActionTypes crated and sessionactions begun

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -1,11 +1,11 @@
 import * as types from './actionTypes';
-import sessionApi from '../api/SessionApi';
+import sessionApi from '../api/SessionApi'; //this handles api requests
 
 export function loginSuccess() {
   return {type: types.LOG_IN_SUCCESS}
 }
 
-export function logInUser(credentials) {
+export function signIn(credentials) { //credentials come from state
   return function(dispatch) {
     return sessionApi.login(credentials).then(response => {
       sessionStorage.setItem('jwt', response.jwt);
