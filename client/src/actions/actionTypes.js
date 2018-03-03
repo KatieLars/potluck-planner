4343dc64829a45674f0f43ed3c5ec01b487aca5c
github username <github email address>
2018-03-02 19:15:13 -0600

    set up Api to sessions

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,1 +1,1 @@
-export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS' //saving constants for easy updating
--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -1,5 +1,5 @@
 import * as types from './actionTypes';
-import sessionApi from '../api/SessionApi'; //this handles api requests
+import SessionApi from '../api/SessionApi'; //this handles api requests
 
 export function loginSuccess() {
   return {type: types.LOG_IN_SUCCESS}
@@ -7,7 +7,7 @@ export function loginSuccess() {
 
 export function signIn(credentials) { //credentials come from state
   return function(dispatch) {
-    return sessionApi.login(credentials).then(response => {
+    return SessionApi.login(credentials).then(response => {
       sessionStorage.setItem('jwt', response.jwt);
       dispatch(loginSuccess());
     }).catch(error => {
