0d14c0101a6948b6ba33b93d88cb6bb427540007
github username <github email address>
2018-03-03 21:04:52 -0600

    navbar set up

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk) )
+
 
 ReactDOM.render(
-  <App />, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 );
