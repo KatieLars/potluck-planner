e8f350e5b3fd5c0b36b9b8f0ed3a87fdebb8126c
github username <github email address>
2018-03-04 14:20:27 -0600

    stub user home page

---
+++ client/src/pages/userHomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UserHomePage extends Component {
+  render() {
+    return(
+      <div>
+        <h1>You are logged in!</h1>
+      </div>
+    )
+  }
+}
--- client/src/reducers/sessionsReducer.js
+++ client/src/reducers/sessionsReducer.js
@@ -1,11 +1,10 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
-import {BrowserRouter} from 'react-router';
+
 
 export default function sessionsReducer(state = initialState.session, action) {
   switch(action.type) {
     case types.LOG_IN_SUCCESS:
-      history.push('/')
       return !!sessionStorage.jwt
     default:
       return state;
