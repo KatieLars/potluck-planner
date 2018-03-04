92b5d6d607ee1410871a99b26e9ac8a0bcbb0c4c
github username <github email address>
2018-03-04 17:43:32 -0600

    knock files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+        post 'user_token' => 'user_token#create'
        resources :potlucks
        resources :users
-       mount Knock::Engine => "/knock"
        resources :guestships
        resources :friendships
        resources :recipes
