e757658aa500bd3110fdc85c1f1dca645991e562
github username <github email address>
2018-03-04 17:40:31 -0600

    change knock namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-  before_action :authenticate_api_v1_user
+  before_action :authenticate
 
 
   private
 
-  def authenticate_api_v1_user
-    authenticate_for Api::V1::User
-  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
 
     class Users < ApplicationController
-      skip_before_action :authentication_api_v1_user, only: [:create]
+      skip_before_action :authenticate, only: [:create]
 
     end
   end
