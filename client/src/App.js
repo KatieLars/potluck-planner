2a8c58262d6f06c7ea25cbf0b7425b7c32a70da6
github username <github email address>
2018-03-03 22:54:43 -0600

    about page route

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import WelcomeNavbar from './components/welcomeNavbar'
 import Home from './components/home'
-
+import About from './components/about'
 
 class App extends Component {
   render() {
@@ -11,6 +11,7 @@ class App extends Component {
         <Router>
           <div>
             <WelcomeNavbar />
+            <Route path="/about" component={About}/>
             <Route exact path="/" component={Home}/>
           </div>
         </Router>
---
+++ client/src/components/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Button, Jumbotron } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+
+
+class About extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default About
