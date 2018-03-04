2c2f6a4a6d4b0d5736e281bb8353117c7f658d45
github username <github email address>
2018-03-03 22:57:39 -0600

    about page

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Jumbotron } from 'reactstrap'
+import { Container, Col, Row, Button, Jumbotron } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 
@@ -8,7 +8,21 @@ class About extends Component {
   render() {
     return(
       <div>
-
+      <Jumbotron style={{textAlign: "center"}}>
+  <h1>Potlucks . . . Upgraded!</h1>
+  <Container>
+    <Row>
+      <Col className="col-3"></Col>
+      <Col className="col-6"><p>Potluck planner takes your food-sharing game to the next level.
+      Now you can track not only who's coming, but what they're bringing.
+      Avoid guests bringing the same dishes (one can only eat so much potato salad, after all),
+      get ideas for portable foods when you're at a loss of what to bring, and track your own
+      contributions to the time-honored tradition of bringing a dish to pass.
+      </p></Col>
+      <Col className="col-3"></Col>
+    </Row>
+  </Container>
+</Jumbotron>
       </div>
     )
   }
