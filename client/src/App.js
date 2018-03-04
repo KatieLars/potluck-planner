83bd0d9bf3210cca80a544e8a27324a924ba6182
github username <github email address>
2018-03-04 14:24:34 -0600

    import route components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import WelcomeNavbar from './components/welcomeNavbar'
 import Home from './components/home'
 import About from './components/about'
+import SignIn from './components/signin'
+import UserHomePage from './pages/userHomePage'
 
 class App extends Component {
   render() {
@@ -14,7 +16,6 @@ class App extends Component {
             <Route path="/about" component={About}/>
             <Route exact path="/" component={Home}/>
             <Route path="/signin" component={SignIn}/>
-            <Route path="/signup" component={SignUp}/>
             <Route path="/home" component={UserHomePage}/>
           </div>
         </Router>
@@ -24,3 +25,5 @@ class App extends Component {
 }
 
 export default App;
+
+    <Route path="/signup" component={SignUp}/>
