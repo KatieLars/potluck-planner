d8d1901c9e1f11f511d5cfcf3bcf4650fd5fadb5
github username <github email address>
2018-03-03 21:11:43 -0600

    debug syntax

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './reducers/index'
+import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk) )
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import potlucksReducer from './potlucksReducer';
 
 export default combineReducers({
-  potlucks: potlucksReducers
+  potlucks: potlucksReducer
 });
