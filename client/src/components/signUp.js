3fcd48a6523a27ad31ffbcf605ab54f8c62854c4
github username <github email address>
2018-03-03 23:10:03 -0600

    stub out sign up form

---
+++ client/src/components/signUp.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {Form, FormGroup, Col, Row, Container, Button, Label, Input} from 'reactstrap'
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import * as sessionActions from '../actions/sessionActions'
+
+class SignUp extends Component {
+  constructor() {
+    super()
+    this.state = {
+      info: {
+        password: "",
+        passwordConfirmation: "",
+        username: "",
+        email: "",
+      }
+    }
+  }
+
+handleChange(event){
+  const field = event.target.name;
+  const info = this.state.info;
+  info[field] = event.target.value;
+  return this.setState({info: info})
+  }
+
+handleSubmit(event) {
+  event.preventDefault()
+  this.props.actions.signUp(this.state.info);
+}
+
+render() {
+return(
+    <Container>
+      <Row style={{paddingTop: "25px"}}>
+        <Col className="col-4"></Col>
+        <Col className="col-4">
+          <Form onChange={(event) => this.handleChange(event)} onSubmit={(event) => this.handleSubmit(event)}>
+            <FormGroup>
+              <Label for="username">Username</Label>
+              <Input type="username" name="username" id="username" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" name="email" id="email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input type="password" name="password" id="password" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="passwordConfirmation">Password Confirmation</Label>
+              <Input type="password" name="passwordConfirmation" id="passwordConfirmation" />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </Col>
+        <Col className="col-4"></Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default SignIn
