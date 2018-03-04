360ece304cf01b76565b17767596ac0334e4cd99
github username <github email address>
2018-03-04 14:15:12 -0600

    add sessionreducer to rootreducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import potlucksReducer from './potlucksReducer';
+import sessionsReducer from './sessionsReducer';
 
 export default combineReducers({
-  potlucks: potlucksReducer
+  potlucks: potluckReducer,
+  sessions: sessionsReducer,
 });
