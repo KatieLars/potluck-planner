229fa102dfc76cd19a036e3af186c11b40a1fd43
github username <github email address>
2018-03-04 21:42:02 -0600

    format userNavbar

--- client/src/components/userNavbar.js
+++ client/src/components/userNavbar.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { Component} from 'react';
+import { Navbar, Nav, NavItem, NavLink, NavbarBrand } from 'reactstrap';
 import { NavLink as TopNav } from 'react-router-dom'
 
-const UserNavbar = () => {
+ class UserNavbar extends Component {
+  render() {
   return(
     <div>
     <Navbar className="navbar navbar-expand-lg bg-dark" >
-      <NavbarBrand><TopNav to="/">Home</TopNav></NavbarBrand>
+      <NavbarBrand><TopNav to="/:user_id/signout">Sign Out</TopNav></NavbarBrand>
         <Nav navbar className="list-unstyled ml-auto" >
           <NavItem>
             <NavLink>
-              <TopNav to="/signup">Sign Up</TopNav>
+              <TopNav to="/:user_id/potlucks">Your Potlucks</TopNav>
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink>
-              <TopNav to="/about">About</TopNav>
+              <TopNav to="/:user_id/recipes">Your Recipes</TopNav>
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink>
-              <TopNav to="/signin">Sign In</TopNav>
+              <TopNav to="/:user_id/friends">Friends</TopNav>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>
+              <TopNav to="/:user_id/account">Account</TopNav>
             </NavLink>
           </NavItem>
         </Nav>
       </Navbar>
     </div>
-)}
+)}}
 
 export default UserNavbar
