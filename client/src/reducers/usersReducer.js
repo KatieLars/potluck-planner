6539fc62234aaea2ad19424e1e997be76486dfe4
github username <github email address>
2018-03-06 16:28:34 -0600

    clear user from state

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -8,6 +8,8 @@ export default function usersReducer(state = initialState.user, action) {
       return {...state, user: action.user}
     case types.FOUND_USER:
       return {...state, user: action.user}
+    case types.LOG_OUT_SUCCESS:
+      return {...state, user: {}} 
     default:
       return state;
   }
