ecce6c65873d3bcdebe8ed9cf80f0f85d1e23c17
github username <github email address>
2018-03-06 14:19:03 -0600

    new find route for finding user by email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,17 @@ class UsersController < ApplicationController
       end
     end
 
+    def find
+      @user = User.find_by(email: params[:user][:email])
+      binding.pry
+      if @user
+        render json:@user
+      else
+        @errors = @user.errors.full_messages
+        render json:@errors
+      end
+    end
+
 
   private
 
