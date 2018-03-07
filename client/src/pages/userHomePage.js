6e12811caa03bd88f82d99375123aceedb023e8b
github username <github email address>
2018-03-06 18:39:59 -0600

    Jumbotron

--- client/src/pages/userHomePage.js
+++ client/src/pages/userHomePage.js
@@ -3,28 +3,23 @@ import {connect} from 'react-redux';
 import Navigation from '../containers/navigation'
 import {bindActionCreators} from 'redux';
 import * as sessionActions from '../actions/sessionActions'
+import { Jumbotron } from 'reactstrap'
+import stillLife from '../images/stillLife.jpg'
 
+const backgroundImage = {
+  backgroundImage: "url(" + { stillLife } + ")"
+}
 
 class UserHomePage extends Component {
-
-  componentWillMount() {
-  
-     // if(this.props.user.users.user) {
-     //   const credentials = {email: this.props.user.users.user.email, password: this.props.user.users.user.password }
-     //   this.props.actions.signIn(credentials)
-     // }
-  }
-
   render() {
-
      const username = this.props.user.users.user.username
-    // const email = this.props.user.users.user.email
-    // const image = this.props.user.users.user.image
+     const email = this.props.user.users.user.email
+     const image = this.props.user.users.user.image
     return(
       <div>
-
-        <h1>Welcome {username}!</h1>
-
+        <Jumbotron style={{textAlign: "center"}} style={{backgroundImage}}>
+          <h1>Welcome {username}!</h1>
+        </Jumbotron>
       </div>
     )
   }
