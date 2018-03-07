8cf40aeaa12d2bbfac4be0ef7b87e619f380fdce
github username <github email address>
2018-03-07 14:15:12 -0600

    final css home

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
-import { Button, Jumbotron } from 'reactstrap'
+import { Button, Jumbotron, Container, Row, Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
+import danielSpoerri from '../images/danielSpoerri.jpg'
 
+const homeStyle = {
+  opacity: "0.75",
+  width: "60%",
+  textAlign: "center",
+  color: "black",
+}
 
+const homeBackground = {
+  paddingLeft: "50px",
+  paddingTop: "125px",
+  backgroundImage: "url(" + danielSpoerri + ")",
+  backgroundSize: "cover",
+  height: "100vh",
+  width: "100%",
+}
 class Home extends Component {
 
   render() {
 
     return(
       <div>
-        <Jumbotron style={{textAlign: "center"}} >
-          <h1>Potluck Planner</h1>
-          <p className="lead"><em>What's happening, who's coming, and what's to eat!</em></p>
+        <Container style={homeBackground}>
+        <Row>
+        <Col className="col-2"></Col>
+        <Jumbotron style={homeStyle} >
+          <h1><strong>Potluck Planner</strong></h1>
+          <p className="lead"><em><strong>What's happening, who's coming, and what's to eat!</strong></em></p>
           <hr className="my-4"/>
-          <p>Set a date. Invite friends. Bring food. And track it all.</p>
+          <p><strong>Set a date. Invite friends. Bring food. And track it all.</strong></p>
           <Button><Link to="/about">Learn More</Link></Button>
         </Jumbotron>
+        </Row>
+        </Container>
       </div>
     )
   }
