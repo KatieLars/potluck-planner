ffb20cbee6debbc71883ff2acc62d467d4ab74d5
github username <github email address>
2018-03-06 19:13:22 -0600

    Css for homepage jumbotron

--- client/src/pages/userHomePage.js
+++ client/src/pages/userHomePage.js
@@ -3,11 +3,14 @@ import {connect} from 'react-redux';
 import Navigation from '../containers/navigation'
 import {bindActionCreators} from 'redux';
 import * as sessionActions from '../actions/sessionActions'
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron, Container } from 'reactstrap'
 import stillLife from '../images/stillLife.jpg'
 
 const backgroundImage = {
-  backgroundImage: "url(" + { stillLife } + ")"
+  backgroundImage: "url(" + stillLife + ")",
+  height: "100vh",
+  textAlign: "center",
+  color: "white",
 }
 
 class UserHomePage extends Component {
@@ -16,9 +19,11 @@ class UserHomePage extends Component {
      const email = this.props.user.users.user.email
      const image = this.props.user.users.user.image
     return(
-      <div>
-        <Jumbotron style={{textAlign: "center"}} style={{backgroundImage}}>
-          <h1>Welcome {username}!</h1>
+      <div className="container-full-bg">
+        <Jumbotron  style={backgroundImage}>
+          <Container>
+            <h1>Welcome {username}!</h1>
+          </Container>
         </Jumbotron>
       </div>
     )
