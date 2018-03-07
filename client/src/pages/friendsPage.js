82398057016e51185af62e8ede4a26bdd597756f
github username <github email address>
2018-03-07 15:13:17 -0600

    stub out friends list

--- client/src/pages/friendsPage.js
+++ client/src/pages/friendsPage.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react'
+import friendsList from '../components/friendsList'
+import {Button} from 'reactstrap'
 
 class FriendsPage extends Component {
+
+  componentWillMount() {
+    //makes a get request to Friendships/index to get friends
+  }
+
+  handleClick(event) {
+    //this will make an get request and produce a pop up window with a list of allusers
+    //will need a redirect in action to popup
+  }
+
+
   render() {
-    return(
-      <div>
-      </div>
-    )
+    if (this.props.friends) {
+      return(
+        <div>
+          <friendsList friends={this.props.friends}/>
+          <Button onClick={(event) => handleClick(event)}>
+        </div>
+      )
+    }else {
+      return(
+        <div>
+          <h1>Be friendly!<h1>
+          <Button onClick={(event) => handleClick(event)}>Add Friends</Button>
+        </div>
+      )
+    }
   }
 
 }
