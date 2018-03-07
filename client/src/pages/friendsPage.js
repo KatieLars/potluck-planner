dfb6840a445c74d334a0417056dd880819bf7a8c
github username <github email address>
2018-03-07 15:04:28 -0600

    stub out friends page

---
+++ client/src/pages/friendsPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class FriendsPage extends Component {
+  render() {
+    return(
+      <div>
+      </div>
+    )
+  }
+
+}
+
+export default
+
+//this page must:
+  //display all friends
+  //offer the option to unfriend
+  //add friends
