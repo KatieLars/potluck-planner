e4545fe3cc10727b80ddbb3f4275b14d6981725c
github username <github email address>
2018-03-07 20:23:29 -0600

    debug UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :password, :email, :username, :image
-  has_many :friends
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   def not_friends #grabs all users who are NOT also friends
     User.all.reject{|user| user.id == object.id} - object.friends
