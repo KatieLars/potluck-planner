4112bba54e98d36f033ea37da46ac97c917fc0cb
github username <github email address>
2018-03-07 18:29:14 -0600

    added modal buttons

--- client/src/components/addFriends.js
+++ client/src/components/addFriends.js
@@ -8,19 +8,30 @@ class AddFriends extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userId = this.props.user.users.user.id,
+      userId: this.props.user.users.user.id,
+      notFriends: [],
+      newFriends: [],
     }
   }
 
   componentWillMount() { //may need to use componentDidMount
-    this.props.actions.getNotFriends(this.state.userId)
+    this.setState = {
+      notFriends: this.props.actions.getNotFriends(this.state.userId)
+  }
+
+  addFriendsHandler(event) {
+    event.preventDefault()
+
   }
 
   render() {
     return(
       <div>
         <Modal isOpen="true">
-          <ModalHeader>Hello!</ModalHeader>
+          <ModalHeader>Select Friends</ModalHeader>
+          <ModalBody></ModalBody>
+          <ModalFooter>
+            <Button onClick={(event) => this.addFriendsHandler(event)}>Add Friends<
         </Modal>
       </div>
     )
