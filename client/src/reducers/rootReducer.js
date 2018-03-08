3ddcde79f572fb5442df89ec1f5c3229c0644a81
github username <github email address>
2018-03-08 13:18:24 -0600

    wiring friendsReducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import potlucksReducer from './potlucksReducer';
 import sessionsReducer from './sessionsReducer';
 import usersReducer from './usersReducer'
+import friendsReducer from './friendsReducer'
 
 export default combineReducers({
+  friends: friendsReducer,
   users: usersReducer,
   potlucks: potlucksReducer,
   sessions: sessionsReducer,
