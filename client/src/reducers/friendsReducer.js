365efa951d46c2ea9d33a2dfa3aee1b36e6f8220
github username <github email address>
2018-03-08 15:02:08 -0600

    moving debuggers

--- client/src/reducers/friendsReducer.js
+++ client/src/reducers/friendsReducer.js
@@ -5,7 +5,6 @@ import {BrowserRouter} from 'react-router-dom'
 export default function usersReducer(state = initialState.friends, action) {
     switch(action.type) {
     case types.NOT_FRIENDS:
-    debugger
       return {...state, friends: {notFriends: action.notFriends.other_users}}
     default:
       return state;
