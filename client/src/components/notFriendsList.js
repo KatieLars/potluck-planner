e059b8d64eef69732c355061242ab52bd4925ef1
github username <github email address>
2018-03-07 18:56:02 -0600

    stub out NotFriendsList

--- client/src/components/notFriendsList.js
+++ client/src/components/notFriendsList.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react'
+import {Input, Form, FormGroup}
+
+const imageStyle = {
+  width: "10%",
+}
 
 class NotFriendsList extends Component {
+
+  //iterate over props passed in and return username and thumbnail image
+  const notFriendsCheckbox = () => {
+    this.props.not_friends.map((friend) => {
+      return (
+        <Input type="checkbox" value={friend.id} style={imageStyle}>
+            <img src={friend.image} className="img-thumbnail"/>{friend.username}
+         </Input>
+      )
+    })
+  }
+
   render() {
+
     return (
-      
+      <div>
+        {notFriendsCheckbox}
+      </div>
     )
   }
 }
