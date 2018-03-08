2d451afa90a2fd7ea5cb63f9840b27b452c620a1
github username <github email address>
2018-03-08 17:21:37 -0600

    delete duplicate action getNotFriends

--- client/src/api/friendApi.js
+++ client/src/api/friendApi.js
@@ -1,20 +1,4 @@
 class FriendApi {
-  static getNotFriends(userId) {
-    const request = new Request(`http://localhost:3001/users/${info.id}`, {
-      method: 'PATCH',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${sessionStorage.jwt}`,
-      }),
-      body: JSON.stringify({user: info})
-    });
-
-    return fetch(request).then(response => {
-      return response.json();
-    }).catch(error => {
-      return error;
-    });
-  }
 
   static getFriends(userId) { //get current friends
     const request = new Request(`http://localhost:3001/users/${info.id}`, {
