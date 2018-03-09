626f7dcadf1f1730792808d923276b6217136681
github username <github email address>
2018-03-09 10:09:02 -0600

    finish API request

--- client/src/api/friendApi.js
+++ client/src/api/friendApi.js
@@ -21,8 +21,22 @@ class FriendApi {
   }
 
   static removeFriends(friendshipIds) {
-    const request = new Request(`http://localhost:3001/friendships`)
+    const request = new Request(`http://localhost:3001/friendships/destroy_multiple`, {
+      method: 'DELETE',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.jwt}`,
+      }),
+      body: JSON.stringify({friendship: friendshipIds})
+    })
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
   }
+  
 }
 
 export default FriendApi
