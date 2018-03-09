c098c6da318675c7327f51c43d4cd9dccfea8fec
github username <github email address>
2018-03-09 11:51:16 -0600

    debug initial state and reducer

--- client/src/actions/friendsActions.js
+++ client/src/actions/friendsActions.js
@@ -5,7 +5,7 @@ import history from '../history'
 
 export function removeFriends(friendshipIds) {
   return function(dispatch) {
-    return FriendApi.removeFriends(friendshipIds).then(response => 
+    return FriendApi.removeFriends(friendshipIds).then(response => {
       dispatch(removeFriendsSuccess(response));
     }).catch(error => {
       throw(error)
--- client/src/reducers/friendsReducer.js
+++ client/src/reducers/friendsReducer.js
@@ -2,7 +2,7 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 import {BrowserRouter} from 'react-router-dom'
 
-export default function friendsReducer(state = initialState.friends, action) {
+export default function friendsReducer(state = initialState.user, action) {
     switch(action.type) {
     case types.NOT_FRIENDS:
       return {...state, friends: {notFriends: action.notFriends.other_users}}
