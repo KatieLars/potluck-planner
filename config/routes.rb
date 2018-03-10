b569891a94bf174d7089efee648e16bda706a248
github username <github email address>
2018-03-09 21:58:36 -0600

    change route for not_friends to friendships

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
        post 'user_token' => 'user_token#create'
+
        get '/friendships/friends'=> 'friendships#friends'
+       get '/friendships/not_friends' => 'friendships#not_friends'
+       post '/friendships/end_friendships' => 'friendships#end_friendships'
+
+       post 'find_user' => 'users#find'
+
        resources :potlucks
        resources :users
        resources :guestships
        resources :friendships
        resources :recipes
-       post 'find_user' => 'users#find'
-       get '/:user_id/not_friends' => 'users#not_friends'
-       post '/friendships/end_friendships' => 'friendships#end_friendships'
 end
