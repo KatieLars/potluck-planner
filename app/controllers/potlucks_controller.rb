dee5bb0027d01cad8e562f7a0da6cee5d716a063
github username <github email address>
2018-03-12 14:30:53 -0500

    making reducer action

--- app/controllers/potlucks_controller.rb
+++ app/controllers/potlucks_controller.rb
@@ -1,6 +1,6 @@
 class PotlucksController < ApplicationController
 
-  def index
+  def index #hosted potlucks and potlucks for which you are invited
     @total_potlucks = current_user.total_potlucks
     render json: @total_potlucks
   end
--- client/src/reducers/potlucksReducer.js
+++ client/src/reducers/potlucksReducer.js
@@ -1,3 +1,11 @@
-export default (state = [], action) => {
-    return state
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+export default (state = initialState.potlucks , action) => {
+  switch(action.type) {
+    case types.POTLUCKS_FOUND:
+      return {...state, potlucks: action.potlucks}
+    default:
+      return state
+    }
 }
