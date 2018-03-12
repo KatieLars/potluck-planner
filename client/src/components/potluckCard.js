a910076a5f7ff8a8d40abcf0fe7ce8bd74ae92d2
github username <github email address>
2018-03-12 11:30:42 -0500

    finish potluck card

--- client/src/components/potluckCard.js
+++ client/src/components/potluckCard.js
@@ -7,7 +7,18 @@ class PotluckCard extends Component {
   render() {
     return (
       <div>
-
+      <Card>
+        <CardImg top width="100%" src={this.props.potluck.image} alt="Potluck Image" />
+          <CardBody>
+            <CardTitle>{this.props.potluck.name}</CardTitle>
+            <CardSubtitle><em>{this.props.potluck.happening}</em></CardSubtitle>
+            <CardText>
+              <p>Location: {this.props.potluck.location}</p>
+              {this.props.potluck.description}
+            </CardText>
+            <Button>More Info</Button>
+          </CardBody>
+      </Card>
       </div>
     )
   }
