fed1237df43606f67495ef2b2caf15b98747d751
github username <github email address>
2018-03-13 16:48:17 -0500

    stub out potluckShow page

--- client/src/components/potluckShow.js
+++ client/src/components/potluckShow.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import PotluckCard from './potluckCard'
+import {Container, Row, Col, Button} from 'reactstrap'
+import history from '../history'
 
-
+const imageStyle ={
+  height: "30%",
+  paddingBottom: "20px"
+}
 
 class PotluckShow extends Component {
+
+  getAttendees(event) => { //modal
+    event.preventDefault()
+    history.push(`/potlucks/${this.props.potluck.id}/attendees`)
+  }
+  inviteGuests(event) => { //modal
+    event.preventDefault()
+    history.push(`/potlucks/${this.props.potluck.id}/guests`)
+  }
 
   render() {
     return (
       <div>
-        <h1>PotluckCard potluck={this.props.potluck}</h1>
+        <Container style={{textAlign: "center"}}>
+          <Row style={{paddingTop: "25px"}}>
+            <Col className="col-3"></Col>
+            <Col className="col-6">
+              <h1>{this.props.potluck.name}</h1>
+              <img src={this.props.potluck.image} style={imageStyle}/>
+              <h3>{this.props.potluck.location}</h3>
+              <p><em>{this.props.potluck.date}</em>{this.props.potluck.time}</p>
+              <p>{this.props.potluck.description}</p>
+              <Button onClick={(event) => {this.inviteGuests(event)}}>Invite Guests</Button>
+              <ButtononClick={(event) => {this.getAttendees(event)}}>See Who is Coming</Button>
+              <Button>Suggest Recipes</Button> //modal with checkboxes
+              <Button>Create A New Recipe</Button> //modal to create a new recipe
+            </Col>
+            <Col className="col-3"></Col>
+          </Row>
+        </Container>
       </div>
     )
   }
