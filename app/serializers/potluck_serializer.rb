0e857379123a31b8f4291dbefdc113977c53c966
github username <github email address>
2018-03-12 23:41:13 -0500

    added time and date formatting

--- app/serializers/potluck_serializer.rb
+++ app/serializers/potluck_serializer.rb
@@ -1,4 +1,12 @@
 class PotluckSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :description, :date, :time, :canceled, :image, :user_id
+  attributes :id, :name, :location, :description, :format_date, :format_time, :canceled, :image, :user_id
   belongs_to :user
+
+  def format_date
+    object.date.strftime("%A %B %e, %Y")
+  end
+
+  def format_time
+    object.time.strftime("%l:%M %P")
+  end
 end
