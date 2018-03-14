26e8e45983cc30536020a08bcb0118979b89a6f7
github username <github email address>
2018-03-14 09:55:11 -0500

    added potluck assocation in recipeserializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,7 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :difficulty, :image, :user_id, :url
   belongs_to :user
+  has_many :potluck_recipes
+  has_many :potlucks, through: :potluck_recipes
 
 end
