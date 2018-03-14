406496b71d8090227950d69548308fbe41fd7eab
github username <github email address>
2018-03-14 11:23:46 -0500

    added recipes association

--- app/serializers/potluck_serializer.rb
+++ app/serializers/potluck_serializer.rb
@@ -3,6 +3,8 @@ class PotluckSerializer < ActiveModel::Serializer
   belongs_to :user
   has_many :guestships
   has_many :guests, through: :guestships
+  has_many :potluck_recipes
+  has_many :recipes, through: :potluck_recipes
 
   def format_date
     if object.date
