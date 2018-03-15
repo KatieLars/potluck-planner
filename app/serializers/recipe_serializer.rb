575c880fc1f297eaaa646d8b7b0346e95aa2b373
github username <github email address>
2018-03-14 20:18:19 -0500

    add cooked_recipe_serial

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,11 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :difficulty, :image, :user_id, :url
+  attributes :id, :name, :difficulty, :image, :user_id, :url, :cooked_recipes
   belongs_to :user
   has_many :potluck_recipes
   has_many :potlucks, through: :potluck_recipes
+
+  def cooked_recipes_serial
+    object.cooked_recipes
+  end
 
 end
