ea8f271162cb56205183fcb483eb3d36f8bfafc6
github username <github email address>
2018-03-15 09:40:05 -0500

    added imports

--- client/src/components/recipeCard.js
+++ client/src/components/recipeCard.js
@@ -31,29 +31,30 @@ deleteUserFromPotluckRecipe(event) {
   //make sure that recipe list is updated if this happens
 }
 
-// potluckLink() {
-//   //needs to find potluck in state based on potluckRecipe id given in props
-//   //const foundPotluck = this.props.potlucks.find(potluck => {
-//   //return potluck.id == this.props.potluckRecipe.potluck_id})
-// //})
-//   //<Link to="/potlucks/${foundPotluck.id}">{foundPotluck.name}</Link>
-// }
+cookRecipe(event) {
+  event.preventDefault()
+  //adds user_id to correct potluck_recipe and re-renders the potluck show page
+}
+
+potluckLink() {
+  //needs to find potluck in state based on potluckRecipe id given in props
+  //const foundPotluck = this.props.potlucks.find(potluck => {
+  //return potluck.id == this.props.potluckRecipe.potluck_id})
+//})
+  //<Link to="/potlucks/${foundPotluck.id}">{foundPotluck.name}</Link>
+}
 
 
   render() {
-
      let bottom = null
      let top = null
-
-    // if(this.props.currentPotluck) {
-    //   if(this.props.recipe && this.props.recipe.claimant_id == this.props.user.id) {
-    //     bottom = <Button onClick={(event) => {this.deleteUserFromPotluckRecipe(event)}}>Fine. Reject Me.</Button>
-    //   }else if(this.props.recipe && this.props.recipe.claimant_id != null) {
-    //     bottom =  <strong>I am Taken!</strong>
-    //   }
-    // }else{
-    //   return null
-    // }
+      if(this.props.recipe.claimant_id == this.props.user.id) {
+        bottom = <Button onClick={(event) => {this.deleteUserFromPotluckRecipe(event)}}>Fine. Reject Me.</Button>
+      }else if(this.props.recipe.claimant_id != null) {
+        bottom =  <strong>I am Taken!</strong>
+      }else{
+        bottom = <Button onClick{(event) => this.cookRecipe(event)}>Cook Me!</Button>
+      }
 debugger
       // if(this.props.recipe.user_id == this.props.user.id || this.props.recipe.recipe && this.props.recipe.recipe.user_id == this.props.user.id) {
       //   top = (
@@ -69,12 +70,12 @@ debugger
     return (
       <Card style={subtitleStyle} >
         {top}
-        <CardImg top width="100%" src={this.props.recipe.recipe.image} alt="Potluck Image" />
-          <CardBody>
-            <CardTitle>{this.props.recipe.recipe.name}</CardTitle>
-            <CardSubtitle style={{fontSize: "0.7em"}}>Difficulty: {this.props.recipe.recipe.difficulty}</CardSubtitle>
-            <CardLink href={this.props.recipe.recipe.url}>Get Recipe</CardLink>
-          </CardBody>
+        // <CardImg top width="100%" src={this.props.recipe.recipe.image} alt="Potluck Image" />
+        //   <CardBody>
+        //     <CardTitle>{this.props.recipe.recipe.name}</CardTitle>
+        //     <CardSubtitle style={{fontSize: "0.7em"}}>Difficulty: {this.props.recipe.recipe.difficulty}</CardSubtitle>
+        //     <CardLink href={this.props.recipe.recipe.url}>Get Recipe</CardLink>
+        //   </CardBody>
           <CardFooter>
             {bottom}
           </CardFooter>
--- client/src/containers/recipePotluck.js
+++ client/src/containers/recipePotluck.js
@@ -1,6 +1,40 @@
 import React, {Component} from 'react'
-
+import {Button, CardHeader, Card, CardLink, CardFooter} from 'reactstrap'
+import RecipeCard from '../components/recipeCard'
 class RecipePotluck extends Component {
+
+  render() {
+    let bottom = null
+    let top = null
+//bottom
+     if(this.props.recipe.claimant_id == this.props.user.id) {
+       bottom = <Button onClick={(event) => {this.deleteUserFromPotluckRecipe(event)}}>Fine. Reject Me.</Button>
+     }else if(this.props.recipe.claimant_id != null) {
+       bottom =  <strong>I am Taken!</strong>
+     }else{
+       bottom = <Button onClick{(event) => this.cookRecipe(event)}>Cook Me!</Button>
+     }
+//top
+     if this.props.recipe.recipe.user_id == this.props.user.id) {
+       top = (
+       <CardHeader className="col d-flex justify-content-center">
+         <CardLink href="#"  style={{display: "inline-block"}} onClick={(event) => this.editRecipe(event)} >Edit Recipe</CardLink>
+         <CardLink href="#" style={{display: "inline-block"}} onClick={(event) => this.deleteRecipe(event)}>Delete Recipe</CardLink>
+       </CardHeader>
+     )}else {
+       top = <CardHeader></CardHeader>
+     }
+
+    return(
+      <Card style={subtitleStyle} >
+          {top}
+        <RecipeCard recipe={this.props.recipe.recipe}/>
+        <CardFooter>
+          {bottom}
+        </CardFooter>
+      </Card>
+    )
+  }
 
 }
 
