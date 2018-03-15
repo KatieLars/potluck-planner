c7c44813c983a2b9c109c33cc7844c648e6a1252
github username <github email address>
2018-03-15 15:23:15 -0500

    create recipe params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,4 +15,10 @@ class RecipesController < ApplicationController
     end
   end
 
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:image, :name, :url, :difficulty)
+  end
+
 end
