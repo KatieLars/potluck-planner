fcce5f5d000c6dcf3c3a788843be15483802a55a
github username <github email address>
2018-03-15 15:49:39 -0500

    reconfiguring return value of api index call

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,16 @@ class Recipe < ApplicationRecord
   has_many :potlucks, through: :potluck_recipes
   belongs_to :user
 
+  def cooked_recipes
+    current_user.list_potluck_recipes
+  end
+
+  def new_recipes #created not cooked
+    current_user.user_created_not_cooked
+  end
+
+  def total_recipes #created and/or cooked
+    current_user.total_user_recipes
+  end
+
 end
