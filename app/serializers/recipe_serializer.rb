a499b6b621c5a45d979cc6714b35cac5607c476c
github username <github email address>
2018-03-14 20:40:58 -0500

    delete cooked_recipe

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,11 +1,7 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :difficulty, :image, :user_id, :url, :cooked_recipes
+  attributes :id, :name, :difficulty, :image, :user_id, :url
   belongs_to :user
   has_many :potluck_recipes
   has_many :potlucks, through: :potluck_recipes
-
-  def cooked_recipes_serial
-    object.cooked_recipes
-  end
 
 end
