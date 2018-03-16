d1edbca3c60f644abbe4a0b83cd2d14548657210
github username <github email address>
2018-03-15 21:58:31 -0500

    clarified state for potlucks

--- client/src/reducers/potlucksReducer.js
+++ client/src/reducers/potlucksReducer.js
@@ -4,10 +4,8 @@ import initialState from './initialState';
 export default (state = initialState.potlucks , action) => {
   switch(action.type) {
     case types.POTLUCKS_FOUND:
-    debugger
-      return {...state, potlucks: action.potlucks}
+      return {...state, potlucks: action.potlucks.potlucks, allPotluckRecipes: action.potlucks.total_potluck_recipes}
     case types.CREATE_POTLUCK:
-
       return {...state, potlucks: [...state, action.newPotluck]}
     default:
       return state
