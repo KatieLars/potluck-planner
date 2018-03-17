19d67f7e03e729be4a9c52adc929b8543c3083de
github username <github email address>
2018-03-17 15:13:37 -0500

    making RecipeChecklist more generic

---
+++ client/src/components/checkList.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import {Label, Form, FormGroup, Input, Container, Row, CardDeck} from 'reactstrap'
+import {connect} from 'react-redux'
+
+const cardDecks = {
+  columnGap: "1rem",
+  columnCount: "3"
+}
+//either selecting guests or selecting recipes
+class CheckList extends Component {
+
+  checklistItem() {
+      return this.props.list.map((item) => (
+        <FormGroup check>
+         <Label check>
+           <Input type="checkbox" value={item.id} />{' '}
+            {item.username ? (item.username) : (item.name)}
+         </Label><br></br>
+       </FormGroup>
+      )
+    )}
+
+  render() {
+    return (
+      <div>
+        {this.checklistItem()}
+      </div>
+    )
+  }
+}
