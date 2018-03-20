5b729941d9fb0a6e81de51c65299051bf1530711
github username <github email address>
2018-03-20 15:17:44 -0500

    make stateless constants

--- client/src/components/potluckForm.js
+++ client/src/components/potluckForm.js
@@ -6,27 +6,27 @@ const PotluckForm = ({ potluck }) => {
     <Col className="col-4" style={{marginLeft: "25px"}}>
       <FormGroup>
         <Label for="name">Name</Label>
-        <Input type="text" name="name" id="name" placeholder={this.props.potluck ? (this.props.potluck.name) : (null)} />
+        <Input type="text" name="name" id="name" placeholder={potluck ? (potluck.name) : (null)} />
       </FormGroup>
       <FormGroup>
         <Label for="location">Location</Label>
-        <Input type="location" name="location" id="location" placeholder={this.props.potluck ? (this.props.potluck.location) : (null)} />
+        <Input type="location" name="location" id="location" placeholder={potluck ? (potluck.location) : (null)} />
       </FormGroup>
       <FormGroup>
         <Label for="date">Date</Label>
-        <Input type="text" name="date" id="date" placeholder={this.props.potluck ? (this.props.potluck.format_date) : (null)} />
+        <Input type="text" name="date" id="date" placeholder={potluck ? (potluck.format_date) : (null)} />
       </FormGroup>
       <FormGroup>
         <Label for="time">Time</Label>
-        <Input type="datetime" name="time" id="time" placeholder={this.props.potluck ? (this.props.potluck.format_time) : (null)}/>
+        <Input type="datetime" name="time" id="time" placeholder={potluck ? (potluck.format_time) : (null)}/>
       </FormGroup>
       <FormGroup>
         <Label for="description">Description</Label>
-        <Input type="text" name="description" id="description" placeholder={this.props.potluck ? (this.props.potluck.description) : (null)}/>
+        <Input type="text" name="description" id="description" placeholder={potluck ? (potluck.description) : (null)}/>
       </FormGroup>
       <FormGroup>
         <Label for="image">Image</Label>
-        <Input type="text" name="image" id="image" placeholder={this.props.potluck ? (this.props.potluck.image) : (null)}/>
+        <Input type="text" name="image" id="image" placeholder={potluck ? (potluck.image) : (null)}/>
       </FormGroup>
     </Col>
   )
--- client/src/components/recipeForm.js
+++ client/src/components/recipeForm.js
@@ -1,59 +1,44 @@
 import React, {Component} from 'react'
 import {FormGroup, Label, Input} from 'reactstrap'
 
-//rendered in:
-  //RecipeModal
-    //update recipe from recipe index
-    //update recipe from potluck show
-    //create recipe from potluck show
-  //NewRecipePage
-//can take a recipe as props for placeholder values
-class RecipeForm extends Component {
-
-isChecked(difficulty){
-    this.props.recipe.difficulty == `${difficulty}` ? ("active") : (null)
-  }
-
-  render() {
+const RecipeForm = ({ recipe }) => {
     return (
       <div>
         <FormGroup>
           <Label for="name">Name</Label>
-          <Input type="text" name="name" id="name" placeholder={this.props.recipe ? (this.props.recipe.name) : (null)} />
+          <Input type="text" name="name" id="name" placeholder={recipe ? (recipe.name) : (null)} />
         </FormGroup>
         <FormGroup check className="form-check-inline">
           <Label>Difficulty: </Label>
         </FormGroup>
         <FormGroup check className="form-check-inline">
           <Label check >
-          <Input type="radio" name="difficulty" value="easy" checked={this.props.recipe ? (this.isChecked("easy")) : (null)}/>
+          <Input type="radio" name="difficulty" value="easy" />
           Easy
           </Label>
         </FormGroup>
         <FormGroup check className="form-check-inline">
-          <Label check active={this.props.recipe ? (this.props.recipe.difficulty == "moderate" ? (true) : (false)) : (null)}>
+          <Label check>
           <Input type="radio" name="difficulty" value="moderate" />
           Moderate
           </Label>
         </FormGroup>
         <FormGroup check className="form-check-inline">
-          <Label check active={this.props.recipe ? (this.props.recipe.difficulty == "hard" ? (true) : (false)) : (null)}>
+          <Label check >
           <Input type="radio" name="difficulty" value="hard" />
           Hard
           </Label>
         </FormGroup>
         <FormGroup>
           <Label for="url">URL:</Label>
-          <Input type="text" name="url" id="url" placeholder={this.props.recipe ? (this.props.recipe.url) : (null)} />
+          <Input type="text" name="url" id="url" placeholder={recipe ? (recipe.url) : (null)} />
         </FormGroup>
         <FormGroup>
           <Label for="image">Image</Label>
-          <Input type="text" name="image" id="image" placeholder={this.props.recipe ? (this.props.recipe.image) : (null)}/>
+          <Input type="text" name="image" id="image" placeholder={recipe ? (recipe.image) : (null)}/>
         </FormGroup>
       </div>
     )
-  }
-
 }
 
 export default RecipeForm
