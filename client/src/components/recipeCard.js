71da5cfbd36da605f4aef639506a8c4e1f3c1470
github username <github email address>
2018-03-20 15:27:43 -0500

    syntax fixes

--- client/src/components/recipeCard.js
+++ client/src/components/recipeCard.js
@@ -4,11 +4,11 @@ import { CardLink, CardImg, CardBody, CardTitle, CardSubtitle} from 'reactstrap'
 const RecipeCard = ({ recipe }) => {
     return (
       <div>
-        <CardImg top width="100%" src={this.props.recipe.image} alt="Potluck Image" />
+        <CardImg top width="100%" src={recipe.image} alt="Recipe Image" />
           <CardBody>
-            <CardTitle>{this.props.recipe.name}</CardTitle>
-            <CardSubtitle style={{fontSize: "0.7em"}}>Difficulty: {this.props.recipe.difficulty}</CardSubtitle>
-            <CardLink href={this.props.recipe.url}>Get Recipe</CardLink>
+            <CardTitle>{recipe.name}</CardTitle>
+            <CardSubtitle style={{fontSize: "0.7em"}}>Difficulty: {recipe.difficulty}</CardSubtitle>
+            <CardLink href={recipe.url}>Get Recipe</CardLink>
           </CardBody>
       </div>
     )
