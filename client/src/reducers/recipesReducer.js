a12ad607e6e0a1736b6ce684d170ff5405592110
github username <github email address>
2018-03-20 00:23:02 -0500

    create recipe filter if values are null

--- client/src/reducers/recipesReducer.js
+++ client/src/reducers/recipesReducer.js
@@ -1,12 +1,19 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
+const filterRecipes = (recipes) => {
+  return recipes.filter(recipe => recipe !== null)
+}
+
 export default (state = initialState.recipes , action) => {
+
 
   switch(action.type) {
     case types.RECIPES_FOUND:
-    debugger
-      return {...state, allRecipes: action.recipes.all_recipes, userRecipes: {newRecipes: action.recipes.user_recipes.new_recipes, cookedRecipes: action.recipes.user_recipes.cooked_recipes, totalRecipes: action.recipes.user_recipes.total_recipes}}
+      return {...state, allRecipes: filterRecipes(action.recipes.all_recipes),
+        userRecipes: {newRecipes: filterRecipes(action.recipes.user_recipes.new_recipes),
+                      cookedRecipes: filterRecipes(action.recipes.user_recipes.cooked_recipes),
+                      totalRecipes: filterRecipes(action.recipes.user_recipes.total_recipes)}}
     default:
       return state
     }
