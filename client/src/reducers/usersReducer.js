7a5aa18862012d46189c7ace2fc998ceb50bfe8a
github username <github email address>
2018-03-27 15:51:28 -0500

    delete NEW_USER_SUCCESS

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -3,9 +3,7 @@ import initialState from './initialState';
 import {BrowserRouter} from 'react-router-dom'
 
 export default function usersReducer(state = initialState.user, action) {
-    switch(action.type) {
-    case types.NEW_USER_SUCCESS:
-      return {...state, user: action.user}
+    switch(action.type) { //change return value to {...state, action.user}
     case types.FOUND_USER:
       return {...state, user: action.user}
     case types.UPDATE_USER:
