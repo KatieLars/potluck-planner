7c93cb71c9bfced6f6ca4219e134af18d060e759
github username <github email address>
2018-03-27 19:47:49 -0500

    added delete_all

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
-  has_many :potluck_recipes
+  has_many :potluck_recipes, :dependent => :delete_all
   has_many :potlucks, through: :potluck_recipes
   belongs_to :user
-  dependent: delete_all
+
 
 end
