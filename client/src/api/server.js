4d604ec1266fb38666235f4f2dce2ff71fe84cc5
github username <github email address>
2018-06-16 19:22:01 -0500

    clean up server.js

--- client/src/api/server.js
+++ client/src/api/server.js
@@ -1,4 +1,2 @@
-// export const SERVER = "https://git.heroku.com/potluckers.git"
-
 export const TEST_SERVER = "http://localhost:3001"
 export const PRODUCTION_SERVER = "https://git.heroku.com/potluckers.git"
