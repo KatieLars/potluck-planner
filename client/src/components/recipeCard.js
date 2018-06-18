88a8dcdfa1cb8a58bfed4e7e70c566b6d0803e78
github username <github email address>
2018-06-17 19:15:25 -0500

    deleting voting functionality

--- client/src/components/recipeCard.js
+++ client/src/components/recipeCard.js
@@ -2,48 +2,48 @@ import React, {Component} from 'react'
 import { CardLink, CardImg, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap'
 
 class RecipeCard extends Component {
-  constructor() {
-    super()
-    this.state = {
-      votes: 0
-    }
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     votes: 0
+  //   }
 
     //this.handleClick = this.handleClick.bind(this)
-  }
+  // }
+  //
+  //
+  // handleClick = () => {
+  //   this.setState({
+  //     votes: ++this.state.votes
+  //   })
+  // }
 
-
-  handleClick = () => {
-    this.setState({
-      votes: ++this.state.votes
-    })
-  }
-
-  callApi = () => {
-    console.log('a')
-    fetch('http://localhost:3001/friendships/friendsjsjdk', {
-      method: 'GET',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${sessionStorage.jwt}`,
-      })
-    }).then(response => {
-        if(!response.ok) {
-          throw new Error(response.statusText)
-        }else{
-        console.log('b')
-        return response.json()
-      }
-    }).then(response => {
-      console.log('c', response)
-    }).catch(error => {
-      console.log('d', error);
-    })
-    console.log('e')
+  // callApi = () => {
+  //   console.log('a')
+  //   fetch('http://localhost:3001/friendships/friendsjsjdk', {
+  //     method: 'GET',
+  //     headers: new Headers({
+  //       'Content-Type': 'application/json',
+  //       'Authorization': `Bearer ${sessionStorage.jwt}`,
+  //     })
+  //   }).then(response => {
+  //       if(!response.ok) {
+  //         throw new Error(response.statusText)
+  //       }else{
+  //       console.log('b')
+  //       return response.json()
+  //     }
+  //   }).then(response => {
+  //     console.log('c', response)
+  //   }).catch(error => {
+  //     console.log('d', error);
+  //   })
+  //   console.log('e')
 
     // a e b c + response
 
     // a e d + error
-  }
+  // }
 
   render() {
     const { recipe } = this.props
@@ -54,9 +54,6 @@ class RecipeCard extends Component {
             <CardTitle>{recipe.name}</CardTitle>
             <CardSubtitle style={{fontSize: "0.7em"}}>Difficulty: {recipe.difficulty}</CardSubtitle>
             <CardLink href={recipe.url}>Get Recipe</CardLink>
-            <Button onClick={this.handleClick}>Vote Up! </Button>
-            <Button onClick={this.callApi}>Call Api</Button>
-            <p>Votes: {this.state.votes}</p>
           </CardBody>
       </div>
     )
